5775d5063ae6120af20b55c78b54fe539b898977
Peter Emil Jensen <ubs908@gmail.com>
2017-03-21 15:25:32 +0100

    Removed uncorrect parts

--- python/blockchain/node.py
+++ python/blockchain/node.py
@@ -7,21 +7,9 @@ class Node():
     def __init__(self, address, port):
         self._address = address
         self._port = port
-
-        self._blockchain = []
-        self._loafs = []
         self._nodes = []
 
-    def add_block(block):
-        self._blockchain.append(block)
     def broadcast_loaf(loaf):
-        self._loafs.append(loaf)
-
-    def attach_block_validator():
-        pass
-    def attach_loaf_validator():
-        pass
-    def attach_consensus():
         pass
 
     def connect_node():
@@ -29,7 +17,7 @@ class Node():
         self.query_length()
         if local_length < queried_length:
             self.request_blocks()
-        pass
+
     def query_length():
         pass
     def request_blocks():
@@ -37,12 +25,10 @@ class Node():
     def received_block(block):
         # validate block
         # check if new
-        self.add_block(block)
         # forward block
         pass
     def received_loaf(loaf):
         # validate loaf
         # check if new
-        self.add_loaf(loaf)
         # forward loaf
         pass
