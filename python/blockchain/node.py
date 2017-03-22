9663b52766627963f5160d81b8ae6d8591507f6a
Peter Emil Jensen <ubs908@gmail.com>
2017-03-22 12:31:46 +0100

    Second try on using autobahn

--- python/blockchain/node.py
+++ python/blockchain/node.py
@@ -1,10 +1,14 @@
+from threading import Thread
 import asyncio
 
 from blockchain.chain import Chain
 from blockchain.loaf import Loaf
 from blockchain.block import Block
+from blockchain.protocol import *
 
 class Node():
+    _singleton = None
+
     def __init__(self, port):
         self._port = port
         self._nodes = set()
@@ -12,11 +16,15 @@ class Node():
         self._chain = Chain()
         self._loaf_pool = {}
 
+        loop = asyncio.new_event_loop()
+        self._server_thread = Thread(target=self._start_server_thread,
+                                     args=(loop,), daemon=True)
+
     def start(self):
-        self._thread.start()
+        self._server_thread.start()
 
     def broadcast_loaf(self, loaf):
-        pass
+        print ("HEJ MED JER ALLE")
 
     def connect_node(self):
         # establish connection
@@ -38,4 +46,25 @@ class Node():
         # check if new
         # forward loaf
         pass
+
+    def _start_server_thread(self, loop):
+        asyncio.set_event_loop(loop)
+        factory = WebSocketServerFactory(u"ws://127.0.0.1:9000")
+        factory.protocol = ServerProtocol
+
+        coro = loop.create_server(factory, '0.0.0.0', self._port)
+        server = loop.run_until_complete(coro)
+
+        try:
+            loop.run_forever()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            server.close()
+            loop.close()
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._singleton:
+            cls._singleton = super(OneOnly, cls)
+        return cls._singleton
 
--- python/blockchain/protocol.py
+++ python/blockchain/protocol.py
@@ -1,6 +1,29 @@
-import asyncio
 from autobahn.asyncio.websocket import WebSocketServerProtocol, \
     WebSocketServerFactory
 from autobahn.asyncio.websocket import WebSocketClientProtocol, \
     WebSocketClientFactory
+
+from blockchain.node import Node
+
+class ServerProtocol(WebSocketServerProtocol):
+
+    def onConnect(self, request):
+        print("Client connecting: {0}".format(request.peer))
+        Node().broadcast_loaf(2)
+
+    def onOpen(self):
+        print("WebSocket connection open.")
+        self._node.broadcast_loaf(2)
+
+    def onMessage(self, payload, isBinary):
+        if isBinary:
+            print("Binary message received: {0} bytes".format(len(payload)))
+        else:
+            print("Text message received: {0}".format(payload.decode('utf8')))
+
+        # echo back message verbatim
+        self.sendMessage(payload, isBinary)
+
+    def onClose(self, wasClean, code, reason):
+        print("WebSocket connection closed: {0}".format(reason))
 
