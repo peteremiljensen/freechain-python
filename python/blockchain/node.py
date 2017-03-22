ef3ac4d220d21454126ff22d07dd5430c160bdf5
Peter Emil Jensen <ubs908@gmail.com>
2017-03-22 16:32:49 +0100

    A working queue system

--- python/blockchain/node.py
+++ python/blockchain/node.py
@@ -2,6 +2,7 @@ import threading
 import asyncio
 import websockets
 import queue
+import time
 
 from blockchain.chain import Chain
 from blockchain.loaf import Loaf
@@ -10,8 +11,7 @@ from blockchain.block import Block
 class Node():
     def __init__(self, port):
         self._port = port
-        self._server_nodes = set()
-        self._client_nodes = set()
+        self._nodes = set()
         self._queues = {}
 
         self._chain = Chain()
@@ -24,33 +24,9 @@ class Node():
     def start(self):
         self._server_thread.start()
 
-    def broadcast_loaf(self, loaf):
-        print ("HEJ MED JER ALLE", loaf)
-
-    def connect_node(self):
-        # establish connection
-        self.query_length()
-        if local_length < queried_length:
-            self.request_blocks()
-
-    def query_length(self):
-        pass
-    def request_blocks(self):
-        pass
-    def received_block(self, block):
-        # validate block
-        # check if new
-        # forward block
-        pass
-    def received_loaf(self, loaf):
-        # validate loaf
-        # check if new
-        # forward loaf
-        pass
-
     async def _server(self, websocket, path):
         loop = asyncio.get_event_loop()
-        self._server_nodes.add(websocket)
+        self._nodes.add(websocket)
         recv_queue = queue.Queue()
         send_queue = queue.Queue()
         self._queues[websocket] = (recv_queue, send_queue)
@@ -58,29 +34,31 @@ class Node():
             while True:
                 futures = []
                 send_task = None
+                recv_task = asyncio.ensure_future(websocket.recv())
+                futures.append(recv_task)
                 try:
-                    send_data = send_queue.get_nowait()
-                    send_task = asyncio.ensure_future(websocket.send(send_data))
+                    data = send_queue.get_nowait()
+                    send_task = asyncio.ensure_future(websocket.send())
                     futures.append(send_task)
                 except queue.Empty:
                     pass
 
-                recv_task = asyncio.ensure_future(websocket.recv())
-                futures.append(recv_task)
                 done, pending = await asyncio.wait(
                     futures,
                     return_when=asyncio.FIRST_COMPLETED)
 
                 if recv_task in done:
-                    recv_data = recv_task.result()
-                    recv_queue.put(recv_data)
-                    print("Recieved data")
+                    data = recv_task.result()
+                    recv_queue.put(data)
+                    print(data)
                 else:
                     recv_task.cancel()
-        finally:
-            print ("Unregister")
-            self._server_nodes.remove(websocket)
-            del self._queues[websocket]
+        except:
+            pass
+
+        print ("Unregister")
+        self._nodes.remove(websocket)
+        del self._queues[websocket]
 
     def _start_server_thread(self, loop):
         asyncio.set_event_loop(loop)
