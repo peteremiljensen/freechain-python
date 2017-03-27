f2808c100b10afb1e1e5f2d50e4cf6ed1b21b238
Peter Emil Jensen <ubs908@gmail.com>
2017-03-27 13:23:45 +0200

    Made chain thread-safe and conservative

--- python/blockchain/chain.py
+++ python/blockchain/chain.py
@@ -1,4 +1,5 @@
 import sys
+import threading
 
 from blockchain.loaf import *
 from blockchain.block import *
@@ -7,20 +8,24 @@ class Chain():
     def __init__(self):
         genesis_block = Block.create_block_from_json('{"hash": "000002eae755addb7693ac1a1740d36ecc3c843b21affde7271eeb5a0c8cb6cd", "loafs": [], "nounce": 2104806, "previous_block_hash": "-1", "timestamp": "2017-03-21 14:49:34.218393"}')
         self._chain = [genesis_block]
+        self._chain_lock = threading.RLock()
 
     def add_block(self, block):
-        if block.validate() and \
-           self._chain[-1].get_hash() == block.get_previous_block_hash():
-            self._chain.append(block)
-            return True
-        else:
-            return False
+        with self._chain_lock:
+            if block.validate() and \
+               self._chain[-1].get_hash() == block.get_previous_block_hash():
+                self._chain.append(block)
+                return True
+            else:
+                return False
 
     def get_block(self, height):
-        return self._chain[height]
+        with self._chain_lock:
+            return self._chain[height]
 
     def get_length(self):
-        return len(self._chain)
+        with self._chain_lock:
+            return len(self._chain)
 
     def mine_block(self, loafs):
         timestamp = str(datetime.datetime.now())
