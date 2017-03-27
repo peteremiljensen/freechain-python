95a165616b044efe274a86ce4bbdf27293b6b99e
anderslau <andrslsvndsn@gmail.com>
2017-03-27 12:57:41 +0000

    implemented commands

--- python/main.py
+++ python/main.py
@@ -8,11 +8,27 @@ from blockchain.node import Node
 def main():
     node = Node('9000')
     node.start()
-    ip = input('Connect to: ')
-    node.connect_node(ip)
     while True:
-        data = input('Send data: ')
-        node.broadcast(data)
+        inp = input().split()
+        if inp[0] == "connect":
+            if len(inp) == 2:
+                ip = inp[1]
+                node.connect_node(ip)
+            else:
+                print("Invalid input")
+        elif inp[0] == "send":
+            data = ' '.join(inp[1:])
+            node.broadcast(data)
+        elif inp[0] == "length":
+            
+        else:
+            print("Unknown command:", inp[0])
+                
+    #ip = input('Connect to: ')
+    #node.connect_node(ip)
+    #while True:
+    #    data = input('Send data: ')
+    #    node.broadcast(data)
 
 if __name__ == '__main__':
     main()
