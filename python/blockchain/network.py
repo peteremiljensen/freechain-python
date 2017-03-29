5d8ff575f2271b066bd1d0d438a9e4d3236cd83e
maasdamind <mads.da.mind@hotmail.com>
2017-03-29 15:11:49 +0200

    Added comments to network.py

--- python/blockchain/network.py
+++ python/blockchain/network.py
@@ -16,6 +16,8 @@ from blockchain.common import *
 
 class Network():
     def __init__(self, port):
+        """ Network class constructor
+        """
         self._port = port
         self._nodes = set()
         self._queues = {}
@@ -25,30 +27,46 @@ class Network():
                                                daemon=True)
 
     def start(self):
+        """ Starts a thread for the server
+        """
         self._server_thread.start()
 
     def connect_node(self, ip):
+        """ Connects server to a node
+        """
         threading.Thread(target=self._start_client_thread,
                          args=(ip,), daemon=True).start()
 
     def broadcast(self, data):
+        """ Broadcasts data to connected nodes
+        """
         for queue in list(self._queues.values()):
             queue[1].sync_q.put(data)
 
     def send(self, websocket, data):
+        """ Sends data to specific node
+        """
         if websocket in self._queues:
             self._queues[websocket][1].sync_q.put(data)
 
     def recv_nowait(self, websocket):
+        """ Returns contents of queue. If queue is empty, raises exception
+        """
         return self._queues[websocket][0].sync_q.get_nowait()
 
     def get_queues(self):
+        """ Returns queues
+        """
         return self._queues
 
     async def _server(self, websocket, path):
+        """ Waits for _socket to be called
+        """
         await self._socket(websocket)
 
     async def _client(self, ip):
+        """ Connects to a new node
+        """
         async with websockets.connect('ws://' + ip + ':' + str(self._port)) \
                    as websocket:
             Events.Instance().notify(EVENTS_TYPE.NEW_CLIENT_CONNECTION,
@@ -56,6 +74,8 @@ class Network():
             await self._socket(websocket)
 
     async def _socket(self, websocket):
+        """ Creates two queues. One for sending and one for receiving
+        """
         loop = asyncio.get_event_loop()
         self._nodes.add(websocket)
         recv_queue = janus.Queue(loop=loop)
@@ -89,6 +109,8 @@ class Network():
             del self._queues[websocket]
 
     def _start_server_thread(self):
+        """ Starts a server thread and sets it to run until completion
+        """
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         start_server = websockets.serve(self._server, '0.0.0.0', self._port)
@@ -96,6 +118,8 @@ class Network():
         loop.run_forever()
 
     def _start_client_thread(self, ip):
+        """ Starts a client thread and sets it to run until completion
+        """
         try:
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
