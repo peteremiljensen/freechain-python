7c75753368a02b45ea7cd9fd17118e0cf93535d2
maasdamind <mads.da.mind@hotmail.com>
2017-03-29 13:25:49 +0200

    changed loafs to loaves

--- python/genesis_miner.py
+++ python/genesis_miner.py
@@ -1,14 +1,14 @@
 from blockchain.block import *
 import datetime
 
-loafs = []
+loaves = []
 previous_block = "-1"
 height = 0
 nounce = 0
 block = None
 while True:
     timestamp = str(datetime.datetime.now())
-    block = Block(loafs, height, previous_block, timestamp, nounce)
+    block = Block(loaves, height, previous_block, timestamp, nounce)
     #print(block.get_hash()[:5])
     if block.get_hash()[:7] == '0000000':
         print (block.json())
