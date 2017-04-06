26da2f32c33c3a76815e1f35cf010b6dde56a2ba
Peter Emil Jensen <ubs908@gmail.com>
2017-04-06 15:06:43 +0200

    Preparing for another network implementation

--- python/blockchain/network.py
+++ python/blockchain/network.py
@@ -1,7 +1,6 @@
 import threading
 import asyncio
 import janus
-import websockets
 
 from blockchain.events import Events
 from blockchain.common import *
@@ -55,65 +54,17 @@ class Network():
         """
         return self._queues
 
-    async def _server(self, websocket, path):
-        """ Waits for _socket to be called
-        """
-        await self._socket(websocket)
-
-    async def _client(self, websocket):
-        """ Connects to a new node
-        """
-        #Events.Instance().notify(EVENTS_TYPE.NEW_CLIENT_CONNECTION,
-        #                         websocket)
-        await self._socket(websocket)
-
     async def _socket(self, websocket):
         """ Creates two queues. One for sending and one for receiving
         """
-        print("_socket")
-        data = await websocket.recv()
-        await websocket.send(data)
-        '''self._nodes.add(websocket)
-        recv_queue = janus.Queue()
-        send_queue = janus.Queue()
-        self._queues[websocket] = (recv_queue, send_queue)
-        async def recv():
-            try:
-                while True:
-                    data = await websocket.recv()
-                    await recv_queue.async_q.put(data)
-            except websockets.exceptions.ConnectionClosed:
-                pass
-        async def send():
-            try:
-                while True:
-                    data = await send_queue.async_q.get()
-                    await websocket.send(data)
-            except websockets.exceptions.ConnectionClosed:
-                pass
-
-        recv_task = asyncio.ensure_future(recv())
-        send_task = asyncio.ensure_future(send())
-        await asyncio.wait([recv_task, send_task],
-                           return_when=asyncio.FIRST_COMPLETED)
-
-        print(info("Disconnected"))
-        self._nodes.remove(websocket)
-        del self._queues[websocket]'''
+        pass
 
     def _start_server(self):
         """ Starts a server thread and sets it to run until completion
         """
-        server = websockets.serve(self._server, '0.0.0.0',
-                                  self._port)
-        self._loop.run_until_complete(server)
-        #self._loop.run_forever()
+        pass
 
     def _start_client(self, ip, port):
         """ Starts a client thread and sets it to run until completion
         """
-        client = websockets.connect('ws://' + ip + ':' + str(port))
-        websocket = self._loop.run_until_complete(client)
-        self._loop.run_until_complete(websocket.send("Hello!"))
-        reply = self._loop.run_until_complete(websocket.recv())
-        print(reply)
+        pass
