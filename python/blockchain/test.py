595b62aadb7cddb9e873dc341246a65d90cea7ef
Peter Emil Jensen <ubs908@gmail.com>
2017-04-18 14:11:59 +0200

    Changed test folder setup again

--- python/blockchain/test.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-from test_integration.test_integration_1 import Test_Integration_1
-from test_unit.test_unit_1 import Test_Unit_1
-
---
+++ python/blockchain/test_unit/__init__.py
@@ -0,0 +1,1 @@
+from test_unit_1 import *
