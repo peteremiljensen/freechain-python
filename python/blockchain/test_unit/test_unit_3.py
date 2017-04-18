9fb9ead1ba9a1fc252553a46b3cbc350d7d6d402
Peter Emil Jensen <ubs908@gmail.com>
2017-04-18 16:50:14 +0200

    Finished test for Chain

--- python/blockchain/test_unit/test_unit_3.py
+++ python/blockchain/test_unit/test_unit_3.py
@@ -1,11 +1,33 @@
 import unittest
+import json
 from .. import chain, block
 from .miner import *
 
 class TestChainMethods(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.chain = chain.Chain()
+        self.b_1 = self.chain.mine_block([])
+        self.b_2 = block.Block([], 0, "-1", "2012", 512, "test")
+
+    def test_add_block(self):
+        self.assertTrue(self.chain.add_block(self.b_1))
+        self.assertFalse(self.chain.add_block(self.b_2))
+
+    def test_remove_blocks(self):
+        height = self.chain.get_length()
+        self.chain.remove_blocks(height-1)
+        self.assertEqual(height-1, self.chain.get_length())
+
+    def test_get_block(self):
+        b = self.chain.mine_block([])
+        self.chain.add_block(b)
+        self.assertEqual(b, self.chain.get_block(self.chain.get_length()-1))
+
+    def test_json(self):
+        c = chain.Chain()
+        genesis = json.loads(c.json().decode('utf-8'))[0]
+        self.assertEqual(genesis, json.loads(c._chain[0].json().decode('utf-8')))
 
 if __name__ == '__main__':
     unittest.main()
