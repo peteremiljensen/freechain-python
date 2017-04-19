5cb6c36055440e5e249e8e3ef9c6f4eb1356fc59
Peter Emil Jensen <ubs908@gmail.com>
2017-04-19 15:26:24 +0200

    Added test when branched

--- blockchain/test_integration/test_integration_1.py
+++ blockchain/test_integration/test_integration_1.py
@@ -1,5 +1,5 @@
 import unittest
-import threading
+import threading, time
 from ..node import *
 from ..events import *
 from ..common import *
@@ -22,19 +22,22 @@ class TestIntegration1(unittest.TestCase):
         cls.node_1 = Node(9000)
         cls.node_2 = Node(9001)
         cls.node_3 = Node(9002)
+        cls.node_4 = Node(9003)
         cls.node_1.start()
         cls.node_2.start()
         cls.node_3.start()
+        cls.node_4.start()
+        time.sleep(1)
 
         cls.node_1.connect_node('localhost', 9001)
         cls.is_connected = cls.connect_sema.acquire(timeout=20)
 
         cls.loaf = Loaf("test123")
 
-    def test_1_connect_node(self):
+    def test_a_connect_node(self):
         self.assertTrue(self.is_connected)
 
-    def test_2_add_loaf(self):
+    def test_b_add_loaf(self):
         self.assertTrue(self.node_1.add_loaf(self.loaf))
         exists = False
         for l in self.node_1._loaf_pool:
@@ -43,7 +46,7 @@ class TestIntegration1(unittest.TestCase):
                 break
         self.assertTrue(exists)
 
-    def test_3_add_loaf_twice(self):
+    def test_c_add_loaf_twice(self):
         self.assertFalse(self.node_1.add_loaf(self.loaf))
         times = 0
         for l in self.node_1._loaf_pool:
@@ -52,13 +55,13 @@ class TestIntegration1(unittest.TestCase):
                 break
         self.assertEqual(times, 1)
 
-    def test_4_broadcast_loaf(self):
+    def test_d_broadcast_loaf(self):
         self.node_1.broadcast_loaf(self.loaf)
         loaf_sema = threading.Semaphore(0)
         received_loaf = None
         def loaf_callback(loaf):
-            loaf_sema.release()
             received_loaf = loaf
+            loaf_sema.release()
         self.e.register_callback(EVENTS_TYPE.RECEIVED_LOAF, loaf_callback)
         self.assertTrue(loaf_sema.acquire(timeout=20))
 
@@ -69,20 +72,20 @@ class TestIntegration1(unittest.TestCase):
                 break
         self.assertTrue(exists)
 
-    def test_5_mining(self):
+    def test_e_mining(self):
         global block
         block = self.node_1.mine()
         self.assertEqual(block.get_loaves()[0].get_hash(),
                          self.loaf.get_hash())
         self.assertTrue(block.validate())
 
-    def test_6_add_block(self):
+    def test_f_add_block(self):
         global block
         self.assertTrue(self.node_1.add_block(block))
         new_block = self.node_1._chain.get_block(1)
         self.assertEqual(new_block.get_hash(), block.get_hash())
 
-    def test_7_loaf_added_twice_mined(self):
+    def test_g_loaf_added_twice_mined(self):
         self.assertFalse(self.node_1.add_loaf(self.loaf))
         times = 0
         for l in self.node_1._loaf_pool:
@@ -91,7 +94,7 @@ class TestIntegration1(unittest.TestCase):
                 break
         self.assertEqual(times, 0)
 
-    def test_8_broadcast_block(self):
+    def test_h_broadcast_block(self):
         global block
         self.node_1.broadcast_block(block)
         block_sema = threading.Semaphore(0)
@@ -105,7 +108,7 @@ class TestIntegration1(unittest.TestCase):
         new_block = self.node_2._chain.get_block(1)
         self.assertEqual(new_block.get_hash(), block.get_hash())
 
-    def test_9_replacing_chain(self):
+    def test_i_replacing_chain(self):
         connect_sema = threading.Semaphore(0)
         def connection_callback(websocket):
             connect_sema.release()
@@ -118,7 +121,7 @@ class TestIntegration1(unittest.TestCase):
         replaced_sema = threading.Semaphore(0)
         def replaced_callback(websocket):
             replaced_sema.release()
-        self.e.register_callback(EVENTS_TYPE.REPLACED_CHAIN,
+        self.e.register_callback(EVENTS_TYPE.BLOCKS_ADDED,
                                  replaced_callback)
 
         self.assertTrue(replaced_sema.acquire(timeout=20))
@@ -126,6 +129,35 @@ class TestIntegration1(unittest.TestCase):
         for i in range(2):
             self.assertEqual(self.node_1._chain.get_block(i).get_hash(),
                              self.node_3._chain.get_block(i).get_hash())
+
+    def test_j_longer_chain(self):
+        block_1 = self.node_4.mine()
+        self.assertTrue(self.node_4.add_block(block_1))
+        block_2 = self.node_4.mine()
+        self.assertTrue(self.node_4.add_block(block_2))
+        block_3 = self.node_4.mine()
+        self.assertTrue(self.node_4.add_block(block_3))
+
+        connect_sema = threading.Semaphore(0)
+        def connection_callback(websocket):
+            connect_sema.release()
+        self.e.register_callback(EVENTS_TYPE.CONNECTION_READY,
+                                 connection_callback)
+
+        self.node_1.connect_node('localhost', 9003)
+        self.assertTrue(connect_sema.acquire(timeout=20))
+
+        replaced_sema = threading.Semaphore(0)
+        def replaced_callback(data):
+            replaced_sema.release()
+        self.e.register_callback(EVENTS_TYPE.BLOCKS_ADDED,
+                                 replaced_callback)
+
+        self.assertTrue(replaced_sema.acquire(timeout=20))
+
+        for i in range(4):
+            self.assertEqual(self.node_1._chain.get_block(i).get_hash(),
+                             self.node_4._chain.get_block(i).get_hash())
 
 if __name__ == '__main__':
     unittest.main()
