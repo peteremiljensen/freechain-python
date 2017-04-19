eea613141759a16f02d923aeb8e84842326dd141
Peter Emil Jensen <ubs908@gmail.com>
2017-04-19 11:16:46 +0200

    Finished test for Singleton class

--- python/blockchain/test_unit/test_unit_5.py
+++ python/blockchain/test_unit/test_unit_5.py
@@ -1,12 +1,17 @@
 import unittest
 from .. import singleton
 
-@Singleton
+@singleton.Singleton
 class SingletonTest:
     def __init__(self):
         pass
 
 class TestSingletonMethods(unittest.TestCase):
 
-    def setUp(self):
-        pass
+    def test_call_error(self):
+        with self.assertRaises(TypeError):
+            test = SingletonTest()
+
+    def test_instance_check(self):
+        self.assertFalse(isinstance(SingletonTest.Instance(), unittest.TestCase))
+        self.assertTrue(isinstance(SingletonTest.Instance(), SingletonTest))
