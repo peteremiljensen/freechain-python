9488fa912d757c33a66d84c8119235c41957e47e
Peter Emil Jensen <ubs908@gmail.com>
2017-04-19 12:17:53 +0200

    Finished testing for new connection

--- python/blockchain/test_integration/test_integration_1.py
+++ python/blockchain/test_integration/test_integration_1.py
@@ -1,2 +1,26 @@
 import unittest
+import threading
+from ..node import *
+from ..events import *
+from ..common import *
 
+class TestIntegration1(unittest.TestCase):
+
+    def setUp(self):
+        self.connect_sema = threading.Semaphore(0)
+        self.e = Events.Instance()
+        def connection_callback(websocket):
+            self.connect_sema.release()
+        self.e.register_callback(EVENTS_TYPE.CONNECTION_READY,
+                                 connection_callback)
+
+        self.node_1 = Node(9000)
+        self.node_2 = Node(9001)
+        self.node_1.start()
+        self.node_2.start()
+
+        self.node_1.connect_node('localhost', 9001)
+        self.is_connected = self.connect_sema.acquire(timeout=20)
+
+    def test_connect_node(self):
+        self.assertTrue(self.is_connected)
