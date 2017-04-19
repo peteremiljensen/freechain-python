e988aebd7f8a085e60029f4e3a52ae3b7bbb84f1
Peter Emil Jensen <ubs908@gmail.com>
2017-04-19 12:49:32 +0200

    Fixed error regarding severaltime execution of setup

--- python/blockchain/test_integration/test_integration_1.py
+++ python/blockchain/test_integration/test_integration_1.py
@@ -3,24 +3,38 @@ import threading
 from ..node import *
 from ..events import *
 from ..common import *
+#from .. import loaf
+#from .. import block
 
 class TestIntegration1(unittest.TestCase):
 
-    def setUp(self):
-        self.connect_sema = threading.Semaphore(0)
-        self.e = Events.Instance()
+    @classmethod
+    def setUpClass(cls):
+        cls.connect_sema = threading.Semaphore(0)
+        cls.e = Events.Instance()
         def connection_callback(websocket):
-            self.connect_sema.release()
-        self.e.register_callback(EVENTS_TYPE.CONNECTION_READY,
+            cls.connect_sema.release()
+        cls.e.register_callback(EVENTS_TYPE.CONNECTION_READY,
                                  connection_callback)
 
-        self.node_1 = Node(9000)
-        self.node_2 = Node(9001)
-        self.node_1.start()
-        self.node_2.start()
+        cls.node_1 = Node(9000)
+        cls.node_2 = Node(9001)
+        cls.node_1.start()
+        cls.node_2.start()
 
-        self.node_1.connect_node('localhost', 9001)
-        self.is_connected = self.connect_sema.acquire(timeout=20)
+        cls.node_1.connect_node('localhost', 9001)
+        cls.is_connected = cls.connect_sema.acquire(timeout=20)
 
     def test_connect_node(self):
         self.assertTrue(self.is_connected)
+
+    def test_add_loaf(self):
+        pass
+        #loaf = Loaf("test123")
+        #self.assertTrue(self.node_1.add_loaf(loaf))
+        '''exists = False
+        for l in self.node_1._loaf_pool:
+            if l == loaf:
+                exists = True
+                break
+        self.assertTrue(exists)'''
--- python/blockchain/test_unit/test_unit_4.py
+++ python/blockchain/test_unit/test_unit_4.py
@@ -5,33 +5,34 @@ from .. import events
 
 class TestEventsMethods(unittest.TestCase):
 
-    def setUp(self):
-        self.e = events.Events.Instance()
-        self.assertion = False
-        self.data = ""
-        self.assertion_2 = False
-        self.sema = threading.Semaphore(0)
-        self.sema_2 = threading.Semaphore(0)
-        self.sema_3 = threading.Semaphore(0)
+    @classmethod
+    def setUpClass(cls):
+        cls.e = events.Events.Instance()
+        cls.assertion = False
+        cls.data = ""
+        cls.assertion_2 = False
+        cls.sema = threading.Semaphore(0)
+        cls.sema_2 = threading.Semaphore(0)
+        cls.sema_3 = threading.Semaphore(0)
 
         def callback(data):
             if data == 'data':
-                self.assertion = True
-                self.data = data
-                self.sema.release()
+                cls.assertion = True
+                cls.data = data
+                cls.sema.release()
         def callback_2(data):
             if data == 'data_2':
-                self.sema_2.acquire(timeout=20)
-                self.assertion_2 = True
-                self.data = data
-                self.sema_3.release()
-        self.e.register_callback("test", callback)
-        self.e.register_callback("test", callback_2)
-        self.e.notify("test", "data")
-        self.e.notify("test", "data_2")
+                cls.sema_2.acquire(timeout=20)
+                cls.assertion_2 = True
+                cls.data = data
+                cls.sema_3.release()
+        cls.e.register_callback("test", callback)
+        cls.e.register_callback("test", callback_2)
+        cls.e.notify("test", "data")
+        cls.e.notify("test", "data_2")
 
-        self.thread = threading.Thread(target=self.e.start, daemon=True)
-        self.thread.start()
+        cls.thread = threading.Thread(target=cls.e.start, daemon=True)
+        cls.thread.start()
 
     def test_callback(self):
         self.sema.acquire(timeout=20)
