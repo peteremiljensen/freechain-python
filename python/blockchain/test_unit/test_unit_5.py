e75b2c2aedc4ebe7bedf1f68e10618b03ffbe13c
Peter Emil Jensen <ubs908@gmail.com>
2017-04-19 11:07:35 +0200

    Started test_unit_5 implementation

--- python/blockchain/test_unit/test_unit_5.py
+++ python/blockchain/test_unit/test_unit_5.py
@@ -1,2 +1,12 @@
 import unittest
 from .. import singleton
+
+@Singleton
+class SingletonTest:
+    def __init__(self):
+        pass
+
+class TestSingletonMethods(unittest.TestCase):
+
+    def setUp(self):
+        pass
